09218fbe635133eda23adcf88b95538f09c18f34
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-08 18:53:30 +0300

    was done ex3

--- WpfApp_08_06/MainWindow.xaml.cs
+++ WpfApp_08_06/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,51 @@ using System.Windows.Shapes;
 
 namespace WpfApp_08_06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            fibonacciListBox.Items.Clear();
+
+            int limit;
+            if (!int.TryParse(limitTextBox.Text, out limit) || limit < 0)
+            {
+                MessageBox.Show("Please enter a valid non-negative integer for the limit.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            await Task.Run(() =>
+            {
+                var fibonacciSequence = GenerateFibonacciSequence(limit);
+                foreach (var number in fibonacciSequence)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        fibonacciListBox.Items.Add(number);
+                    });
+                }
+            });
+        }
+
+        private List<BigInteger> GenerateFibonacciSequence(int limit)
+        {
+            List<BigInteger> sequence = new List<BigInteger>();
+            BigInteger a = 0;
+            BigInteger b = 1;
+            sequence.Add(a);
+            while (b <= limit)
+            {
+                sequence.Add(b);
+                BigInteger temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return sequence;
         }
     }
 }
