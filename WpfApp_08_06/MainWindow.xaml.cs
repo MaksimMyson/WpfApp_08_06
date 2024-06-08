ee5c95700029cfd7fe932866f6b2951fdc631582
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-08 18:37:06 +0300

    was done ex6

--- WpfApp_08_06/MainWindow.xaml.cs
+++ WpfApp_08_06/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,50 @@ using System.Windows.Shapes;
 
 namespace WpfApp_08_06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(numberTextBox.Text, out int number))
+            {
+                if (number < 0)
+                {
+                    resultLabel.Content = "Number must be non-negative.";
+                    return;
+                }
+
+                try
+                {
+                    BigInteger factorial = await CalculateFactorialAsync(number);
+                    resultLabel.Content = $"Factorial of {number} is {factorial}";
+                }
+                catch (Exception ex)
+                {
+                    resultLabel.Content = $"Error: {ex.Message}";
+                }
+            }
+            else
+            {
+                resultLabel.Content = "Invalid number.";
+            }
+        }
+
+        private Task<BigInteger> CalculateFactorialAsync(int n)
+        {
+            return Task.Run(() =>
+            {
+                BigInteger result = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            });
         }
     }
 }
